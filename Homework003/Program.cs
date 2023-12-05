4dd6b85f4a874dd0b97e18f7749d3e49aa04fa16
Mikhail <wavednight@gmail.com>
2023-12-05 20:42:09 +0700

    Задача3

--- Homework003/Program.cs
+++ Homework003/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] array = { 15,78,98,85,11 };
+
+        Console.WriteLine("Исходный массив: ");
+        PrintArray(array);
+
+        ReverseArray(array);
+
+        Console.WriteLine("Перевернутый массив: ");
+        PrintArray(array);
+    }
+
+    static void ReverseArray(int[] array)
+    {
+        int start = 0;
+        int end = array.Length - 1;
+
+        while (start < end)
+        {
+            int temp = array[start];
+            array[start] = array[end];
+            array[end] = temp;
+
+            start++;
+            end--;
+        }
+    }
+
+    static void PrintArray(int[] array)
+    {
+        foreach (int element in array)
+        {
+            Console.Write(element + " ");
+        }
+        Console.WriteLine();
+    }
+}
