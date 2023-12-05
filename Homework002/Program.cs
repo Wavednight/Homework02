965e519c10fa2106fc7a71c9d5501a896066ecb7
Mikhail <wavednight@gmail.com>
2023-12-05 20:35:27 +0700

    Задача2

--- Homework002/Program.cs
+++ Homework002/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] numbers = new int[10];
+
+        Random random = new Random();
+
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = random.Next(100, 1000);
+        }
+
+        Console.WriteLine("Массив чисел: ");
+        foreach (int number in numbers)
+        {
+            Console.Write(number + " ");
+        }
+        Console.WriteLine();
+
+        int count = 0;
+
+        foreach (int number in numbers)
+        {
+            if (number % 2 == 0)
+            {
+                count++;
+            }
+        }
+
+        Console.WriteLine("Количество четных чисел в массиве: " + count);
+    }
+}
